07ec35f497a9f1d0d7e3872c8b9dff45304463e8
Jan Staelens <jan.staelens@skyline.be>
2023-05-09 16:52:14 +0200

    Add project files.

---
+++ SLC-AS-DeleteMeAfter15-05-2023_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SLC-AS-DeleteMeAfter15-05-2023_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("SLC-AS-DeleteMeAfter15-05-2023_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("934349FB-6BBB-4D6D-A86B-AE4A7B73AD43")]
+[assembly: AssemblyVersion("1.0.0.0")]
